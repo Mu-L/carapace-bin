673882f96160c491087fe73f0ff23551baa8abc7
Burner <10004812+BurnerWah@users.noreply.github.com>
2026-02-27 02:54:57 -0600

    Add more bridged completions (#3293)
    * Added `cosign` via cobra
    
    * Added `slsa-verifier` via cobra
    
    * Added `oh-my-posh` via cobra
    
    * Added `gitleaks` via cobra
    
    * Added `exercism` via cobra
    
    * Added `gitsign` via cobra
    
    * Added `cdebug` via cobra
    
    yaaay got interrupted by an oom i love mac os
    
    * Added `kustomize` via cobra
    
    * Added `k9s` via cobra
    
    * Added `aqua` via urfavecli
    
    * Added `ghalint` via urfavecli
    
    * Added `gitlint` via click
    
    * Added `ykman` via click
    
    * Added `vunnel` via click
    
    * Added `cekit` via click
    
    * Added most core ansible commands via argcomplete
    
    `ansible-test` is excluded because I have no clue what to set the description or URL to
    
    argcomplete is also not guaranteed to work depending on how ansible is installed, but this is fine until actual completers are created
    
    * Added `ansible-creator` via argcomplete
    
    * Added `kubebuilder` via cobra
    
    * Added `nox` via argcomplete
    
    * Added `hatch` via click
    
    * Added `litecli` via click
    
    * Added `mycli` via click
    
    * Added `pgcli` via click
    
    * Added `reuse` via click
    
    * Added `k6` via cobra
    
    `carapace --detect` doesn't pick this one up, but it does work, and it uses cobra so idk
    
    * Added `helmfile` via cobra
    
    * Added `pscale` via cobra
    
    * Added `gomplate` via cobra
    
    * Added `act` via cobra
    
    * Added `apko` via cobra
    
    * Added `argo` & `argocd` via cobra
    
    * Added `colima` via cobra
    
    * Added `hcloud` via cobra
    
    * Added `k3sup` via cobra
    
    * Added `pipx` via argcomplete
    
    * Added `pipenv` via click
    
    * Added `trivy` via cobra
    
    * Added `iredis` via click

--- cmd/carapace/cmd/completers/bridges.go
+++ cmd/carapace/cmd/completers/bridges.go
@@ -11,9 +11,84 @@ func knownbridges() map[string]*completer.Completer {
 	// TODO what about different variants of a command
 	// TODO limit to target os
 	m := map[string]*completer.Completer{
+		"act": {
+			Description: "Run GitHub actions locally",
+			Url:         "https://github.com/nektos/act",
+			Variant:     "cobra",
+		},
+		"ansible": {
+			Description: "Define and run a single task ‘playbook’ against a set of hosts",
+			Url:         "https://docs.ansible.com/projects/ansible/latest/cli/ansible.html",
+			Variant:     "argcomplete",
+		},
+		"ansible-config": {
+			Description: "View ansible configuration",
+			Url:         "https://docs.ansible.com/projects/ansible/latest/cli/ansible-config.html",
+			Variant:     "argcomplete",
+		},
+		"ansible-console": {
+			Description: "REPL console for executing Ansible tasks",
+			Url:         "https://docs.ansible.com/projects/ansible/latest/cli/ansible-console.html",
+			Variant:     "argcomplete",
+		},
+		"ansible-creator": {
+			Description: "The fastest way to generate all your ansible content",
+			Url:         "https://docs.ansible.com/projects/creator/",
+			Variant:     "argcomplete",
+		},
+		"ansible-doc": {
+			Description: "Ansible plugin documentation tool",
+			Url:         "https://docs.ansible.com/projects/ansible/latest/cli/ansible-doc.html",
+			Variant:     "argcomplete",
+		},
+		"ansible-galaxy": {
+			Description: "Perform various Role and Collection related operations",
+			Url:         "https://docs.ansible.com/projects/ansible/latest/cli/ansible-galaxy.html",
+			Variant:     "argcomplete",
+		},
+		"ansible-inventory": {
+			Description: "Show Ansible inventory information",
+			Url:         "https://docs.ansible.com/projects/ansible/latest/cli/ansible-inventory.html",
+			Variant:     "argcomplete",
+		},
+		"ansible-playbook": {
+			Description: "Runs Ansible playbooks, executing the defined tasks on the targeted hosts",
+			Url:         "https://docs.ansible.com/projects/ansible/latest/cli/ansible-playbook.html",
+			Variant:     "argcomplete",
+		},
+		"ansible-pull": {
+			Description: "Pulls playbooks from a VCS repo and executes them on target host",
+			Url:         "https://docs.ansible.com/projects/ansible/latest/cli/ansible-pull.html",
+			Variant:     "argcomplete",
+		},
+		"ansible-vault": {
+			Description: "encryption/decryption utility for Ansible data files",
+			Url:         "https://docs.ansible.com/projects/ansible/latest/cli/ansible-vault.html",
+			Variant:     "argcomplete",
+		},
+		"apko": {
+			Description: "Build OCI images from APK packages directly without Dockerfile",
+			Url:         "https://github.com/chainguard-dev/apko",
+			Variant:     "cobra",
+		},
 		"apptainer": {
 			Description: "Application container and unprivileged sandbox platform for Linux",
 			Url:         "https://apptainer.org/",
+			Variant:     "cobra",
+		},
+		"aqua": {
+			Description: "Declarative CLI Version manager",
+			Url:         "https://aquaproj.github.io/",
+			Variant:     "urfavecli",
+		},
+		"argo": {
+			Description: "Workflow Engine for Kubernetes",
+			Url:         "https://argoproj.github.io/",
+			Variant:     "cobra",
+		},
+		"argocd": {
+			Description: "Declarative Continuous Deployment for Kubernetes",
+			Url:         "https://argoproj.github.io/cd/",
 			Variant:     "cobra",
 		},
 		"az": {
@@ -26,9 +101,29 @@ func knownbridges() map[string]*completer.Completer {
 			Url:         "https://www.boundaryproject.io/downloads",
 			Variant:     "complete",
 		},
+		"cdebug": {
+			Description: "A Swiss army knife of container debugging",
+			Url:         "https://github.com/iximiuz/cdebug",
+			Variant:     "cobra",
+		},
+		"cekit": {
+			Description: "Container Evolution Kit",
+			Url:         "https://cekit.io/",
+			Variant:     "click",
+		},
 		"chezmoi": {
 			Description: "Manage your dotfiles across multiple diverse machines, securely",
 			Url:         "https://chezmoi.io/",
+			Variant:     "cobra",
+		},
+		"colima": {
+			Description: "Container runtimes on macOS (and Linux) with minimal setup",
+			Url:         "https://colima.run/",
+			Variant:     "cobra",
+		},
+		"cosign": {
+			Description: "Code signing and transparency for containers and binaries",
+			Url:         "https://github.com/sigstore/cosign",
 			Variant:     "cobra",
 		},
 		"crc": {
@@ -56,14 +151,79 @@ func knownbridges() map[string]*completer.Completer {
 			Url:         "https://docs.digitalocean.com/reference/doctl/",
 			Variant:     "cobra",
 		},
+		"exercism": {
+			Description: "A command-line interface for Exercism",
+			Url:         "https://exercism.org/",
+			Variant:     "cobra",
+		},
 		"flyctl": {
 			Description: "Command line tools for fly.io services",
 			Url:         "https://fly.io/",
 			Variant:     "cobra",
 		},
+		"ghalint": {
+			Description: "GitHub Actions linter",
+			Url:         "https://github.com/suzuki-shunsuke/ghalint",
+			Variant:     "urfavecli",
+		},
+		"gitleaks": {
+			Description: "Gitleaks scans code, past or present, for secrets",
+			Url:         "https://gitleaks.io/",
+			Variant:     "cobra",
+		},
+		"gitlint": {
+			Description: "Linting for your git commit messages",
+			Url:         "https://jorisroovers.com/gitlint/latest/",
+			Variant:     "click",
+		},
+		"gitsign": {
+			Description: "Keyless Git signing using Sigstore",
+			Url:         "https://github.com/sigstore/gitsign",
+			Variant:     "cobra",
+		},
+		"gomplate": {
+			Description: "Process text files with Go templates",
+			Url:         "https://gomplate.ca/",
+			Variant:     "cobra",
+		},
+		"hatch": {
+			Description: "Modern, extensible Python project management",
+			Url:         "https://hatch.pypa.io/latest/",
+			Variant:     "click",
+		},
+		"hcloud": {
+			Description: "A CLI for Hetzner Cloud",
+			Url:         "https://github.com/hetznercloud/cli",
+			Variant:     "cobra",
+		},
+		"helmfile": {
+			Description: "Declaratively deploy Kubernetes Helm Charts",
+			Url:         "https://github.com/helmfile/helmfile",
+			Variant:     "cobra",
+		},
 		"incus": {
 			Description: "Powerful system container and virtual machine manager",
 			Url:         "https://linuxcontainers.org/incus",
+			Variant:     "cobra",
+		},
+		"iredis": {
+			Description: "A Terminal Client for Redis with AutoCompletion and Syntax Highlighting",
+			Url:         "https://iredis.xbin.io/",
+			Variant:     "click",
+		},
+		"k3sup": {
+			Description: "Bootstrap K3s over SSH in < 60s",
+			Url:         "https://github.com/alexellis/k3sup",
+			Variant:     "cobra",
+		},
+		"k6": {
+			Description: "A modern load testing tool, using Go and JavaScript",
+			Url:         "https://k6.io",
+			Variant:     "cobra",
+		},
+		"k9s": {
+			Description: "K9s is a CLI to view and manage your Kubernetes clusters",
+			Url:         "https://k9scli.io/",
 			Variant:     "cobra",
 		},
 		"kcl": {
@@ -81,6 +241,16 @@ func knownbridges() map[string]*completer.Completer {
 			Url:         "https://sw.kovidgoyal.net/kitty/",
 			Variant:     "kitten",
 		},
+		"kubebuilder": {
+			Description: "CLI tool for building Kubernetes extensions and tools",
+			Url:         "https://github.com/kubernetes-sigs/kubebuilder",
+			Variant:     "cobra",
+		},
+		"kustomize": {
+			Description: "Manages declarative configuration of Kubernetes",
+			Url:         "https://github.com/kubernetes-sigs/kustomize",
+			Variant:     "cobra",
+		},
 		"lefthook": {
 			Description: "Git hooks manager",
 			Url:         "https://lefthook.dev/",
@@ -91,6 +261,16 @@ func knownbridges() map[string]*completer.Completer {
 			Url:         "https://lima-vm.io/",
 			Variant:     "cobra",
 		},
+		"litecli": {
+			Description: "CLI for SQLite Databases with auto-completion and syntax highlighting",
+			Url:         "https://litecli.com/",
+			Variant:     "click",
+		},
+		"mycli": {
+			Description: "A MySQL terminal client with auto-completion and syntax highlighting",
+			Url:         "https://www.mycli.net/",
+			Variant:     "click",
+		},
 		"nerdctl": {
 			Description: "Nerdctl is a command line interface for containerd",
 			Url:         "https://github.com/containerd/nerdctl",
@@ -100,6 +280,16 @@ func knownbridges() map[string]*completer.Completer {
 			Description: "Nomad is an easy-to-use, flexible, and performant workload orchestrator",
 			Url:         "https://www.nomadproject.io/",
 			Variant:     "complete",
+		},
+		"nox": {
+			Description: "Flexible test automation for Python",
+			Url:         "https://nox.thea.codes/",
+			Variant:     "argcomplete",
+		},
+		"oh-my-posh": {
+			Description: "A cross platform tool to render your prompt",
+			Url:         "https://ohmyposh.dev/",
+			Variant:     "cobra",
 		},
 		"op": {
 			Description: "1Password CLI",
@@ -112,9 +302,29 @@ func knownbridges() map[string]*completer.Completer {
 			Url:         "https://orbstack.dev/",
 			Variant:     "cobra",
 		},
+		"pgcli": {
+			Description: "Postgres CLI with autocompletion and syntax highlighting",
+			Url:         "https://pgcli.com/",
+			Variant:     "click",
+		},
+		"pipenv": {
+			Description: "Python Development Workflow for Humans",
+			Url:         "https://pipenv.pypa.io/",
+			Variant:     "click",
+		},
+		"pipx": {
+			Description: "Install and execute apps from Python packages",
+			Url:         "https://pipx.pypa.io/",
+			Variant:     "argcomplete",
+		},
 		"podman": {
 			Description: "Simple management tool for pods, containers and images",
 			Url:         "https://podman.io/",
+			Variant:     "cobra",
+		},
+		"pscale": {
+			Description: "A CLI for communicating with PlanetScale's API",
+			Url:         "https://planetscale.com/docs/cli",
 			Variant:     "cobra",
 		},
 		"ramalama": {
@@ -127,9 +337,24 @@ func knownbridges() map[string]*completer.Completer {
 			Url:         "https://rclone.org/",
 			Variant:     "cobra",
 		},
+		"reuse": {
+			Description: "reuse is a tool for compliance with the REUSE recommendations",
+			Url:         "https://reuse.software",
+			Variant:     "click",
+		},
+		"slsa-verifier": {
+			Description: "Verify provenance from SLSA compliant builders",
+			Url:         "https://github.com/slsa-framework/slsa-verifier",
+			Variant:     "cobra",
+		},
 		"talosctl": {
 			Description: "A CLI for out-of-band management of Kubernetes nodes created by Talos",
 			Url:         "https://docs.siderolabs.com/talos/latest/reference/cli",
+			Variant:     "cobra",
+		},
+		"trivy": {
+			Description: "Scanner for vulnerabilities in container images, file systems, and Git repos",
+			Url:         "https://trivy.dev/",
 			Variant:     "cobra",
 		},
 		"toolbox": {
@@ -141,6 +366,16 @@ func knownbridges() map[string]*completer.Completer {
 			Description: "A tool for secrets management",
 			Url:         "https://www.vaultproject.io/",
 			Variant:     "complete",
+		},
+		"vunnel": {
+			Description: "Tool for collecting vulnerability data from various sources",
+			Url:         "https://github.com/anchore/vunnel",
+			Variant:     "click",
+		},
+		"ykman": {
+			Description: "Configure your YubiKey via the command line",
+			Url:         "https://developers.yubico.com/yubikey-manager/",
+			Variant:     "click",
 		},
 	}
 
